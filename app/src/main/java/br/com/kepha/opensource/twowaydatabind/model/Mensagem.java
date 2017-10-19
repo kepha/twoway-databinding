b354cd0abec2cc95ce9e067f374f72e2ce700d45
Stefan Horochovec <stefan.horochovec@gmail.com>
2017-10-19 10:04:40 -0200

    Adicionado suporte ao BaseObservable na classe Mensagem

--- app/src/main/java/br/com/kepha/opensource/twowaydatabind/model/Mensagem.java
+++ app/src/main/java/br/com/kepha/opensource/twowaydatabind/model/Mensagem.java
@@ -1,15 +1,22 @@
 package br.com.kepha.opensource.twowaydatabind.model;
 
-public class Mensagem {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import br.com.kepha.opensource.twowaydatabind.BR;
+
+public class Mensagem extends BaseObservable {
 
     private String descricao;
 
+    @Bindable
     public String getDescricao() {
         return descricao;
     }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+        notifyPropertyChanged(BR.descricao);
     }
 
     public Mensagem() {
