0c33a0b9a6cef9598e0757f6d738446760254777
Stefan Horochovec <stefan.horochovec@gmail.com>
2017-10-19 10:02:01 -0200

    Refactory da MainActivity para suporte ao data binding do EditText

--- app/src/main/java/br/com/kepha/opensource/twowaydatabind/MainActivity.java
+++ app/src/main/java/br/com/kepha/opensource/twowaydatabind/MainActivity.java
@@ -1,11 +1,15 @@
 package br.com.kepha.opensource.twowaydatabind;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import br.com.kepha.opensource.twowaydatabind.databinding.ActivityMainBinding;
+import br.com.kepha.opensource.twowaydatabind.model.Mensagem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +23,25 @@ public class MainActivity extends AppCompatActivity {
      */
     private EditText editText;
 
+    /**
+     * Mensagem
+     */
+    private Mensagem mensagem = new Mensagem("Mensagem de teste");
+
+    /**
+     * ActivityMainBinding
+     */
+    private ActivityMainBinding activityMainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-        // Definição do layout
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setModel(this.mensagem);
 
         // Bind dos objetos da interface
-        editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
 
         // Evento de clique do botão
@@ -36,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Alerta");
-                builder.setMessage(editText.getText());
+                builder.setMessage(mensagem.getDescricao());
                 AlertDialog dialog = builder.create();
                 dialog.show();
             }
