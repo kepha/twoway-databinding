7b8795df525c65252e3adf60fb9145c6f6d5f7d9
Stefan Horochovec <stefan.horochovec@gmail.com>
2017-10-19 09:54:28 -0200

    Adicionada a classe Mensagem

---
+++ app/src/main/java/br/com/kepha/opensource/twowaydatabind/model/Mensagem.java
@@ -0,0 +1,23 @@
+package br.com.kepha.opensource.twowaydatabind.model;
+
+public class Mensagem {
+
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Mensagem() {
+
+    }
+
+    public Mensagem(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
